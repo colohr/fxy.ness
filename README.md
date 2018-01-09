e96ee7783b60f143702c0c6a024b731b8bf7b030
colohr <colohr.code@gmail.com>
2018-01-09 12:59:47 -0600

    initial

---
+++ README.md
@@ -0,0 +1,10 @@
+fxy.ness 
+=============
+Node External Service Source
+
+--
+Note: `fxy.ness` is not finished & should not be used for your projects. 
+-------------
+```
+  npm uninstall sxy
+```
---
+++ package/index.js
@@ -0,0 +1,13 @@
+const ness = {
+	get registered(){ return get_registered() },
+	get register(){ return register },
+	get has(){ return has_registered }
+}
+
+function get_registered(){
+
+}
+
+function set_ness(){
+
+}
